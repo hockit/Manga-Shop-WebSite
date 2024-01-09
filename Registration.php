ad01a29a5c29829edb3b64259b9783b47b524478
hockit <marcelmd@wp.pl>
2024-01-09 23:16:31 +0100

    Create Registration.php

---
+++ Registration.php
@@ -0,0 +1,12 @@
+<?php
+
+    require_once "connect.php";
+
+    $firstName = $_POST['first-name'];
+    $lastName = $_POST['last-name'];
+    $address = $_POST['address'];
+    $phoneNumber = $_POST['phone-number'];
+
+    
+
+?>
